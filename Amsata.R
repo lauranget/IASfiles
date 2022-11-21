1ba706db0e90aa983bb046721e3e5a38d2d94e97
Amsata <amsata_niang@yahoo.fr>
2022-11-21 19:54:06 +0100

    Modification du fichier Laure.R, ajour du fichier Amsata.R et modification du fichier gitignore

---
+++ Amsata.R
@@ -0,0 +1,7 @@
+# Voici un commentaire
+
+nom=c("Amsata","Niang")
+
+for(i in nom){
+  print(i)
+}
--- Laure.R
+++ Laure.R
@@ -1,1 +1,10 @@
 nom_objet= " la voiture"
+
+
+# Voici un commentaire de Amsata
+
+entier=1:10
+
+for(i in entier) {
+  print(i)
+}
